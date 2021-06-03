d9046a5e003012b9b73d962440cee6811ff07261
Raix123 <85275073+Raix123@users.noreply.github.com>
2021-06-03 15:23:36 +0300

    Add files via upload

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+//sinu andmed
+$db_server = 'localhost';
+$db_andmebaas = 'mangupood';
+$db_kasutaja = 'root';
+$db_salasona = 'Reverse5';
+$yhendus = mysqli_connect($db_server, $db_kasutaja, $db_salasona, $db_andmebaas);
+?>
